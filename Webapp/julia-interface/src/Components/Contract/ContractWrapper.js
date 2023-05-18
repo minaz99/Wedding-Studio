a34f23e41ab077411a9e321c05a60c22a424cebf
minaz99 <hanymina43@gmail.com>
2023-05-18 16:32:05 +0200

    Contract component wrapper

--- Webapp/julia-interface/src/Components/Contract/ContractWrapper.js
+++ Webapp/julia-interface/src/Components/Contract/ContractWrapper.js
@@ -2,6 +2,9 @@ import React from "react";
 import { motion } from "framer-motion";
 import { XCircleIcon } from "@heroicons/react/24/outline";
 import ContractDetails from "./ContractDetails";
+import ContractPackages from "./ContractPackages";
+import ContractPayments from "./ContractPayments";
+import ContractComments from "./ContractComments";
 function ContractWrapper(props) {
   return (
     <motion.div
@@ -12,8 +15,8 @@ function ContractWrapper(props) {
         x: 0,
         scale: 1,
       }}
-      transition={{ duration: 1 }}
-      className="rounded-lg  absolute inset-0 p-3 items-center h-screen mx-auto w-fit justify-center  shadow-lg bg-black/50"
+      transition={{ duration: 0.75 }}
+      className="rounded-lg  absolute inset-0 p-3 items-center h-screen mx-auto w-9/12 shadow-md justify-center  bg-black/50 "
     >
       <div className="flex ">
         <div className="flex-1 "></div>
@@ -25,8 +28,22 @@ function ContractWrapper(props) {
           color="white"
         />
       </div>
-      <div className="p-2">
-        <ContractDetails />
+      <div className="space-y-6">
+        <div className="p-2 flex space-x-12">
+          <ContractDetails
+            status="In progress"
+            secondPartyName="Tamara"
+            brideName="Hinata Hyouga"
+            groomName="Naruto Uzumaki"
+            eventType="Wedding"
+            eventPlace="Movenpick"
+          />
+          <div className="space-y-8">
+            <ContractPackages />
+            <ContractPayments />
+          </div>
+        </div>
+        <ContractComments comments="Sasuke will not be able to attend Uzumaki's wedding" />
       </div>
     </motion.div>
   );
