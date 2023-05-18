ba97fa7327f2ec28a4ee31779a0c6653950a5753
minaz99 <hanymina43@gmail.com>
2023-05-18 16:30:46 +0200

    Wrapper for adding new contract

---
+++ Webapp/julia-interface/src/Components/Contract/AddContractWrapper.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AddContract from "./AddContract";
+
+function AddContractWrapper() {
+  return (
+    <div className="rounded-lg   absolute inset-0 p-3 items-center h-screen mx-auto w-6/12 shadow-md justify-center  bg-gray-300/60">
+      <AddContract />
+    </div>
+  );
+}
+
+export default AddContractWrapper;
