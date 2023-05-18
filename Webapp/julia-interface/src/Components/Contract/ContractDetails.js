5b04b2d1bf0d42b0322e911c09d4423ab085dc25
minaz99 <hanymina43@gmail.com>
2023-05-18 16:31:26 +0200

    Contract details section component

--- Webapp/julia-interface/src/Components/Contract/ContractDetails.js
+++ Webapp/julia-interface/src/Components/Contract/ContractDetails.js
@@ -1,49 +1,79 @@
 import React from "react";
+import StatusWrapper from "../Table/StatusWrapper";
+import {
+  BanknotesIcon,
+  BuildingStorefrontIcon,
+  CalendarDaysIcon,
+  ClockIcon,
+  IdentificationIcon,
+  MegaphoneIcon,
+  PhoneIcon,
+  UserCircleIcon,
+  UserGroupIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 
-function ContractDetails() {
+function ContractDetails(props) {
   return (
-    <div className="space-y-3 rounded-lg p-2 bg-white h-fit w-fit">
-      <div className="font-bold text-lg text-black">Om Amina</div>
-      <div className="flex   text-gray-400">
-        Bride name
-        <div className="mx-3 font-semibold">Amina Khalil</div>
-      </div>
-      <div className=" flex text-gray-400">
-        Groom name
-        <div className="mx-3 font-semibold">Roshdy Abaza</div>
-      </div>
-      <div className=" flex text-gray-400">
-        Date of issue
-        <div className="mx-3 font-semibold">12/06/2023</div>
-      </div>
-      <div className="flex  text-gray-400">
-        Type of occassion
-        <div className="mx-3 font-semibold">Wedding</div>
-      </div>
-      <div className="flex  text-gray-400">
-        Event place
-        <div className="mx-3 font-semibold">Movenpick</div>
-      </div>
-
-      <div className="flex  text-gray-400">
-        Phone number
-        <div className="mx-3 font-semibold">69658340</div>
-      </div>
-
-      <div className="flex  text-gray-400">
-        Contract price
-        <div className="mx-3 font-semibold">650 KD</div>
-      </div>
-      <div className="flex  text-gray-400">
-        Due amount
-        <div className="mx-3 font-semibold">300 KD</div>
-      </div>
-      <div className="flex  text-gray-400">
-        Comments
-        <div className="mx-3 font-semibold">
-          Bride wants photographers to arrive 1 hour and 30 mins earlier for
-          photoshoot of the bride before the weeding
+    <div className="space-y-3 rounded-lg shadow-md p-4 w-6/12 bg-white h-fit  ">
+      <div className="font-medium text-xl">{props.secondPartyName}</div>
+      <div className="space-y-3 ">
+        <div className="flex flex-1 space-x-2">
+          <UserIcon height={22} width={22} color="#ec4899" />
+          <div className="text-gray-500">Bride name</div>
+          <div className="rounded-full">{props.brideName}</div>
         </div>
+        <div className="flex space-x-2">
+          <UserCircleIcon height={22} width={22} color="#3b82f6" />
+          <div className="text-gray-500">Groom name</div>
+          <div>{props.groomName}</div>
+        </div>
+        <div className="flex space-x-2">
+          <UserGroupIcon height={22} width={22} color="#78716c" />
+          <div className="text-gray-500">Second Party name</div>
+          <div>{props.groomName}</div>
+        </div>
+      </div>
+      <div className=" space-y-3  ">
+        <div className="flex space-x-2 flex-1">
+          <MegaphoneIcon height={22} width={22} color="#a78bfa" />
+          <div className="text-gray-500">Event type</div>
+          <div>{props.eventType}</div>
+        </div>
+        <div className="flex space-x-2 flex-1">
+          <BuildingStorefrontIcon height={22} width={22} color="#2dd4bf" />
+          <div className="text-gray-500">Event location</div>
+          <div>{props.eventPlace}</div>
+        </div>
+        <div className="flex space-x-2 flex-1">
+          <CalendarDaysIcon height={22} width={22} color="#818cf8" />
+          <div className="text-gray-500">Event date</div>
+          <div>25/05/2023</div>
+        </div>
+      </div>
+
+      <div className="flex space-x-2 flex-1">
+        <IdentificationIcon height={22} width={22} color="#475569" />
+        <div className="text-gray-500">Civil ID</div>
+        <div>2468205820582</div>
+      </div>
+      <div className="flex space-x-2 flex-1">
+        <PhoneIcon height={22} width={22} color="#4ade80" />
+
+        <div className="text-gray-500">Contact</div>
+        <div>696658340</div>
+      </div>
+      <div className="flex space-x-2">
+        <ClockIcon height={22} width={22} color="#475569" />
+        <div className="text-gray-500">Contract Status </div>
+        <div>
+          <StatusWrapper status={props.status} />
+        </div>
+      </div>
+      <div className="flex space-x-2 flex-1">
+        <BanknotesIcon height={22} width={22} color="#7dd3fc" />
+        <div className="text-gray-500">Contract price</div>
+        <div>600 KD</div>
       </div>
     </div>
   );
