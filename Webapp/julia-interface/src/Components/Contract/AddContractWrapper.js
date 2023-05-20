3d977a451fe46c3d3471c8ebbfdec2c87a3207b3
minaz99 <hanymina43@gmail.com>
2023-05-20 14:48:21 +0200

    Wrapper for adding new contract

--- Webapp/julia-interface/src/Components/Contract/AddContractWrapper.js
+++ Webapp/julia-interface/src/Components/Contract/AddContractWrapper.js
@@ -1,10 +1,18 @@
 import React from "react";
-import AddContract from "./AddContract";
+import { XCircleIcon } from "@heroicons/react/24/outline";
 
 function AddContractWrapper() {
   return (
-    <div className="rounded-lg   absolute inset-0 p-3 items-center h-screen mx-auto w-6/12 shadow-md justify-center  bg-gray-300/60">
-      <AddContract />
+    <div className="rounded-lg   absolute inset-0 p-3 items-center h-screen mx-auto w-6/12 shadow-md justify-center  bg-slate-300">
+      <div className="flex">
+        <div className="flex-1"></div>
+        <XCircleIcon
+          height={26}
+          width={26}
+          color="white"
+          className="cursor-pointer"
+        />
+      </div>
     </div>
   );
 }
