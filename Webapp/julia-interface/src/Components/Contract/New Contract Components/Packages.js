69b6290cc06a52c5171029c03cf89ec9ea74ac3c
minaz99 <hanymina43@gmail.com>
2023-05-20 14:50:38 +0200

    Packages component for packages section

---
+++ Webapp/julia-interface/src/Components/Contract/New Contract Components/Packages.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { motion } from "framer-motion";
+function Packages() {
+  return (
+    <motion.div
+      initial={{ opacity: 0.25, x: -75, y: 0, scale: 1 }} //x:200 ,x:0
+      animate={{
+        opacity: 1,
+        y: 0,
+        x: 0,
+        scale: 1,
+      }}
+      transition={{ duration: 1 }}
+      className="rounded-md bg-white mx-auto p-4"
+    >
+      <div className="font-extrabold text-xl">Packages</div>
+    </motion.div>
+  );
+}
+
+export default Packages;
