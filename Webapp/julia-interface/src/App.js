070c8e751afd2ee00f6f1bde2ae390f378a67a37
minaz99 <hanymina43@gmail.com>
2023-05-27 00:51:56 +0200

    Added the new path for component in new folder

--- Webapp/julia-interface/src/App.js
+++ Webapp/julia-interface/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import TableWrapper from "./Components/Table/TableWrapper";
-import ContractWrapper from "./Components/Contract/ContractWrapper";
+import ContractWrapper from "./Components/Contract/Contract Details/ContractWrapper";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { motion } from "framer-motion";
