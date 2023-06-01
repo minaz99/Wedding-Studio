b09aa8cb7dfe1f6f82d3a57ac13206860d3ba117
minaz99 <hanymina43@gmail.com>
2023-06-01 14:19:33 +0200

    Removed table and added the main wrapper instead

--- Webapp/julia-interface/src/App.js
+++ Webapp/julia-interface/src/App.js
@@ -5,33 +5,12 @@ import ContractWrapper from "./Components/Contract/Contract Details/ContractWrap
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { motion } from "framer-motion";
+import MainWrapper from "./Components/MainWrapper";
 function App() {
-  const [showContract, setShowContract] = useState(false);
-  const [initialX, setIntialX] = useState(0);
-  const [finalX, setFinalX] = useState(0);
-  const pressedRow = () => {
-    setIntialX(0);
-    setFinalX(0); //finalX: -250, InitialX: 0 //we need to animate the whole component (Table + Contract so that they move together in the same transition )
-    setShowContract(true);
-  };
-  const resetValues = () => {
-    setIntialX(0);
-    setFinalX(0);
-    setShowContract(false);
-  };
   return (
     <div className="  bg-gray-300 h-screen  ">
       <div className="justify-evenly ">
-        <TableWrapper
-          initialX={initialX}
-          finalX={finalX}
-          pressedRow={pressedRow}
-        />
-        {showContract ? (
-          <ContractWrapper resetValues={resetValues} />
-        ) : (
-          <div></div>
-        )}
+        <MainWrapper />
       </div>
     </div>
   );
