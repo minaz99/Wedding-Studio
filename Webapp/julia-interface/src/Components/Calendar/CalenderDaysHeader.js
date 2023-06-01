d905662809b74a339498dce67e85cbb24125d72c
minaz99 <hanymina43@gmail.com>
2023-06-01 14:20:25 +0200

    Calender Days Header (displays days of the week)

---
+++ Webapp/julia-interface/src/Components/Calendar/CalenderDaysHeader.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function CalenderDaysHeader() {
+  return (
+    <div className="rounded-md justify- p-2 flex  text-white ">
+      <div className="flex-1 text-start">MON</div>
+      <div className="flex-1 text-start">TUE</div>
+      <div className="flex-1 text-start">WED</div>
+      <div className="flex-1 text-start">THU</div>
+      <div className="flex-1 text-start">FRI</div>
+      <div className="flex-1 text-start">SAT</div>
+      <div className="text-start flex-1">SUN</div>
+    </div>
+  );
+}
+
+export default CalenderDaysHeader;
