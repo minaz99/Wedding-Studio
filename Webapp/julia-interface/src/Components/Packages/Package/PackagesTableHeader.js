21a6abd619b65c5e33febfd9dc2928aacac448d3
minaz99 <hanymina43@gmail.com>
2023-06-05 13:20:52 +0200

    Packages table header i.e (package attrinbutes)

---
+++ Webapp/julia-interface/src/Components/Packages/Package/PackagesTableHeader.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function PackagesTableHeader() {
+  return (
+    <div className="flex   rounded-md bg-gray-100 p-2 space-x-8 shadow-md">
+      <div className="flex-1 font-medium text-slate-600 text-center">Name</div>
+      <div className="flex-1 font-medium text-slate-600 text-center">
+        Pictures
+      </div>
+      <div className="flex-1 font-medium text-slate-600 text-center ">
+        Magzine Size
+      </div>
+      <div className="flex-1 font-medium text-slate-600 text-center">
+        Magzine Mini
+      </div>
+      <div className="flex-1 font-medium text-slate-600 text-center">Video</div>
+      <div className="flex-1 font-medium text-slate-600 text-center">
+        Open Photo & Video{" "}
+      </div>
+      <div className="flex-1 font-medium text-slate-600 text-center">
+        Studio
+      </div>
+      <div className="flex-1 font-medium text-slate-600 text-center">Price</div>
+      <div className="flex-1 font-medium text-slate-600 text-center">Edit</div>
+    </div>
+  );
+}
+
+export default PackagesTableHeader;
