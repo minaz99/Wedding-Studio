406e581bb2b98f28ef8324101f69a121b5dcd53a
minaz99 <hanymina43@gmail.com>
2023-07-07 14:52:30 +0200

    Added new arrowDowns + added search bar

--- Webapp/julia-interface/src/Components/Table/TableHeader.js
+++ Webapp/julia-interface/src/Components/Table/TableHeader.js
@@ -9,23 +9,34 @@ import {
 } from "@heroicons/react/24/outline";
 import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import React, { useState } from "react";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import THEventTypeFilter from "./Filters/THEventTypeFilter";
+import THEventLocationFilter from "./Filters/THEventLocationFilter";
+import THContractStatus from "./Filters/THContractStatus";
+import THBrideNameFilter from "./Filters/THBrideNameFilter";
 
 function TableHeader() {
   const [chevronColor, setChevronColor] = useState("gray");
   const [searchColor, setSearchColor] = useState("#9ca3af");
+  const [searchBride, setSearchBride] = useState(false);
   return (
     <div className="flex   bg-gray-100 justify-between rounded-md text-gray-600 border-gray-400 p-2">
       <div className="items-center  flex text-lg font-semibold">
         <UserIcon height={20} width={20} className="mx-2" color="#ec4899" />
-        Bride's name
-        <MagnifyingGlassIcon
-          onMouseEnter={() => setSearchColor("#4b5563")}
-          onMouseLeave={() => setSearchColor("#9ca3af")}
-          height={20}
-          width={20}
-          className="mx-2 cursor-pointer"
-          color={searchColor}
-        />
+        {!searchBride ? <div>Bride's name</div> : <div></div>}
+        {!searchBride ? (
+          <MagnifyingGlassIcon
+            onMouseEnter={() => setSearchColor("#4b5563")}
+            onMouseLeave={() => setSearchColor("#9ca3af")}
+            height={20}
+            width={20}
+            className="mx-2 cursor-pointer"
+            color={searchColor}
+            onClick={() => setSearchBride(true)}
+          />
+        ) : (
+          <THBrideNameFilter setSearchBride={setSearchBride} />
+        )}
       </div>
       <div className="text-lg flex  font-semibold items-center ">
         <MegaphoneIcon
@@ -36,15 +47,9 @@ function TableHeader() {
         />
         Event type
         <div className="flex-1 w-2"></div>
-        <ChevronDownIcon
-          className="mx-2 cursor-pointer "
-          onMouseEnter={() => setChevronColor("black")}
-          onMouseLeave={() => setChevronColor("gray")}
-          color={chevronColor}
-          height={15}
-          width={15}
-        />
+        <THEventTypeFilter />
       </div>
+
       <div className="text-lg items-center flex  font-semibold">
         <BuildingStorefrontIcon
           height={20}
@@ -54,27 +59,13 @@ function TableHeader() {
         />
         Event place
         <div className="flex-1 w-2"></div>
-        <ChevronDownIcon
-          className="mx-2 cursor-pointer "
-          onMouseEnter={() => setChevronColor("black")}
-          onMouseLeave={() => setChevronColor("gray")}
-          color={chevronColor}
-          height={15}
-          width={15}
-        />
+        <THEventLocationFilter />
       </div>
       <div className="items-center flex  text-lg font-semibold">
         <ClockIcon height={20} width={20} className="mx-2" color="#475569" />
         Contract status
         <div className="flex-1 w-2"></div>
-        <ChevronDownIcon
-          className="mx-2 cursor-pointer "
-          onMouseEnter={() => setChevronColor("black")}
-          onMouseLeave={() => setChevronColor("gray")}
-          color={chevronColor}
-          height={15}
-          width={15}
-        />
+        <THContractStatus />
       </div>
       <div></div>
     </div>
