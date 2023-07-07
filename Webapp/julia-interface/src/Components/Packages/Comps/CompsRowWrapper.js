4783e2a807945718e4959f9859850b974bfb167e
minaz99 <hanymina43@gmail.com>
2023-07-07 18:24:30 +0200

    Wrapper for rowInfo and rowEdit for components

---
+++ Webapp/julia-interface/src/Components/Packages/Comps/CompsRowWrapper.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import CompsRowInfo from "./CompsRowInfo";
+import CompsEditableRow from "./CompsEditableRow";
+
+function CompsRowWrapper(props) {
+  const [editRow, setEditRow] = useState(false);
+  return (
+    <div className="rounded-md bg-slate-300 shadow-md  p-2 ">
+      {editRow ? (
+        <CompsEditableRow
+          setEditRow={setEditRow}
+          name={props.name}
+          type={props.type}
+          price={props.price}
+        />
+      ) : (
+        <CompsRowInfo
+          setEditRow={setEditRow}
+          name={props.name}
+          type={props.type}
+          price={props.price}
+        />
+      )}
+    </div>
+  );
+}
+
+export default CompsRowWrapper;
