20eab79415a8b5980a50eee3e5a23e5804cc8362
minaz99 <hanymina43@gmail.com>
2023-07-10 17:03:14 +0200

    Added add new Frame + edit frames

--- Webapp/julia-interface/src/Components/Packages/Frame/Frames.js
+++ Webapp/julia-interface/src/Components/Packages/Frame/Frames.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import FrameTableHeader from "./FrameTableHeader";
-import FrameRow from "./FrameRow";
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import FrameRowWrapper from "./FrameRowWrapper";
+import FrameEditableRow from "./FrameEditableRow";
 function Frames() {
+  const [newRow, setNewRow] = useState(false);
   return (
     <div className="bg-white rounded-lg p-4   space-y-6">
       <div
@@ -13,14 +15,29 @@ function Frames() {
       </div>
       <div className="space-y-2">
         <FrameTableHeader />
-        <FrameRow name={1} size={"50x70"} price="50" />
-        <FrameRow name={1} size={"50x70"} price="50" />
+        <FrameRowWrapper name={1} size={"50x70"} price="50" />
+        <FrameRowWrapper name={1} size={"50x70"} price="50" />
+        <div className="rounded-md bg-slate-300 shadow-md  p-2 ">
+          {newRow ? (
+            <FrameEditableRow
+              new={true}
+              setNewRow={setNewRow}
+              name=""
+              size=""
+              price=""
+            />
+          ) : (
+            <div></div>
+          )}
+        </div>
       </div>
+
       <PlusCircleIcon
         height={26}
         width={26}
         color="#475569"
         className="mx-auto cursor-pointer"
+        onClick={() => setNewRow(true)}
       />
     </div>
   );
