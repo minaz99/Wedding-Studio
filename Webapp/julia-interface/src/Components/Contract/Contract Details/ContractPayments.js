4eca38df7285c6bbf858e013aa471bb51443948c
minaz99 <hanymina43@gmail.com>
2023-08-14 19:17:03 +0300

    Contract payments compnt with pay and progress bar

--- Webapp/julia-interface/src/Components/Contract/Contract Details/ContractPayments.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/ContractPayments.js
@@ -1,70 +1,84 @@
 import { CreditCardIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
 import { ProgressBar } from "react-bootstrap";
 import PaymentWrapper from "./PaymentWrapper";
-import { useGetContractPaymentsQuery } from "../../../services/api/contractSlice";
+import {
+  useGetContractPaymentsQuery,
+  useMakePaymentMutation,
+} from "../../../services/api/contractSlice";
+import { useState } from "react";
+import GetContractPayment from "../../Table/Contracts Fetching Components/GetContractPayment";
 function ContractPayments(props) {
-  const { data, isLoading, isError, isSuccess } = useGetContractPaymentsQuery({
-    token: props.token,
-    id: props.id,
-  });
+  const [pay, setPay] = useState(false);
+  const [amount, setAmount] = useState(0);
+  const [makePayment, result] = useMakePaymentMutation();
+  const payAmount = async () => {
+    await makePayment({
+      token: props.token,
+      id: props.id,
+      body: { amount: amount },
+    });
+    setPay(false);
+  };
+
   return (
     <div className="space-y-3 rounded-lg p-4   bg-white h-fit  ">
       <div className=" flex  items-center ">
         <div className="font-medium text-xl flex-1">Payments</div>
-        <PencilSquareIcon
-          height={22}
-          width={22}
-          color="#475569"
-          className="cursor-pointer"
-        />
       </div>
 
-      <div className="space-x-4 flex justify-center">
-        {isLoading ? (
+      <div className="">
+        {result.isLoading ? (
           <div className="text-center text-blue-400 text-xl p-4">
             Loading...
           </div>
-        ) : isError ? (
+        ) : result.isError ? (
           <div className="text-center text-red-400 text-xl p-4">
-            Error loading packages
+            Error making payment
           </div>
-        ) : isSuccess ? (
-          data.payments.map((payment) => {
-            return (
-              <div>
-                <PaymentWrapper
-                  paymentNumber={payment.paymentnumber}
-                  amount={payment.amount}
-                />
-              </div>
-            );
-          })
         ) : (
-          <div></div>
+          <GetContractPayment
+            token={props.token}
+            id={props.id}
+          ></GetContractPayment>
         )}
       </div>
-      {isSuccess ? (
-        <div>
-          <ProgressBar
-            now={data.paid}
-            variant="danger"
-            label={data.paid}
-            max={data.paid + data.due}
-          />
-          <div className="flex">
-            <div className="text-gray-500 flex-1 flex">
-              Due amount
-              <div className="mx-2 text-black">{data.due}</div>
-            </div>
 
-            <div className="text-gray-500 flex">
-              Total amount
-              <div className="mx-2 text-black">{data.paid + data.due}</div>
-            </div>
+      {pay ? (
+        <div className="justify-center flex space-x-2">
+          {" "}
+          <input
+            value={amount}
+            style={{
+              background: "#e2e8f0",
+              borderRadius: "6px",
+              border: "none",
+              color: "#475569",
+              padding: "3px 3px 3px 3px",
+              outline: "none",
+            }}
+            className="font-medium "
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <div
+            onClick={() => payAmount()}
+            className="bg-green-400 hover:bg-green-500 p-1.5 font-medium text-lg text-center w-1/6 rounded-md text-white cursor-pointer"
+          >
+            Pay
+          </div>
+          <div
+            onClick={() => setPay(false)}
+            className="bg-slate-500 hover:bg-slate-600 p-1.5 font-medium text-lg text-center w-fit rounded-md text-white cursor-pointer"
+          >
+            Cancel
           </div>
         </div>
       ) : (
-        <div></div>
+        <div
+          onClick={() => setPay(true)}
+          className="bg-blue-400 hover:bg-blue-500 p-1.5 font-medium text-lg text-center w-fit rounded-lg text-white mx-auto cursor-pointer"
+        >
+          Make Payment
+        </div>
       )}
     </div>
   );
