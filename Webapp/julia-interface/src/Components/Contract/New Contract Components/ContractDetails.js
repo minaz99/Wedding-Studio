e8ff50df93592fc16f1e25ff47259f49ef8171a0
minaz99 <hanymina43@gmail.com>
2023-08-19 20:39:20 +0300

    moved the overflow and width to the motion.div

--- Webapp/julia-interface/src/Components/Contract/New Contract Components/ContractDetails.js
+++ Webapp/julia-interface/src/Components/Contract/New Contract Components/ContractDetails.js
@@ -25,9 +25,9 @@ function ContractDetails(props) {
         scale: 1,
       }}
       transition={{ duration: 1 }}
-      className="rounded-md bg-white mx-auto p-4"
+      className="rounded-md w-3/4 overflow-y-scroll bg-white mx-auto p-4"
     >
-      <div className="space-y-3 w-3/4 overflow-y-scroll ">
+      <div className="space-y-3">
         <div className="font-extrabold text-xl">Contract Details</div>
         <div className="flex justify-evenly">
           <div>
