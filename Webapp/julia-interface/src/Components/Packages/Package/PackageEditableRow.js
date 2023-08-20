926951ee4e085df4fdd7e820277e4fd2eff921c0
minaz99 <hanymina43@gmail.com>
2023-08-21 02:26:59 +0300

    sdknf

--- Webapp/julia-interface/src/Components/Packages/Package/PackageEditableRow.js
+++ Webapp/julia-interface/src/Components/Packages/Package/PackageEditableRow.js
@@ -66,121 +66,113 @@ function PackageEditableRow(props) {
   };
 
   return (
-    <div className="flex flex-row">
-      <div className="flex flex-row justify-start ">
-        <input
-          value={name}
-          className="outline-none   rounded-md p-1      border-none hover:bg-gray-300 bg-white"
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Name"
-          style={{ width: "8%" }}
-        />
+    <div className="flex flex-row justify-evenly ">
+      <input
+        value={name}
+        className="outline-none   rounded-md p-1      border-none hover:bg-gray-300 bg-white"
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name"
+        style={{ width: "8%" }}
+      />
 
-        <input
-          value={pics}
-          className="outline-none   mx-4 rounded-md  p-1    border-none hover:bg-gray-300 bg-white"
-          onChange={(e) => setPics(e.target.value)}
-          placeholder="Pics #"
-          style={{ width: "8%" }}
-        />
+      <input
+        value={pics}
+        className="outline-none   mx-4 rounded-md  p-1    border-none hover:bg-gray-300 bg-white"
+        onChange={(e) => setPics(e.target.value)}
+        placeholder="Pics #"
+        style={{ width: "8%" }}
+      />
 
-        <input
-          value={size}
-          className="outline-none   rounded-md p-1   border-none hover:bg-gray-300 bg-white"
-          onChange={(e) => setSize(e.target.value)}
-          placeholder="Magazine"
-          style={{ width: "8%" }}
-        />
-        <div className="flex flex-row justify-evenly">
-          <div className="space-y-1 ">
-            <DropdownButton
-              variant="primary"
-              title={albumCrystal ? "Yes" : "No"}
-            >
-              <Dropdown.Item onClick={() => setAlbumCrystal(true)}>
-                Yes
-              </Dropdown.Item>
-              <Dropdown.Item onClick={() => setAlbumCrystal(false)}>
-                No
-              </Dropdown.Item>
-            </DropdownButton>
-          </div>
-          <div className="space-y-1 ">
-            <DropdownButton
-              variant="primary"
-              title={magazineMini ? "Yes" : "No"}
-            >
-              <Dropdown.Item onClick={() => setMagazineMini(true)}>
-                Yes
-              </Dropdown.Item>
-              <Dropdown.Item onClick={() => setMagazineMini(false)}>
-                No
-              </Dropdown.Item>
-            </DropdownButton>
-          </div>
-          <div className="space-y-1  ">
-            <DropdownButton variant="primary" title={video ? "Yes" : "No"}>
-              <Dropdown.Item onClick={() => setVideo(true)}>Yes</Dropdown.Item>
-              <Dropdown.Item onClick={() => setVideo(false)}>No</Dropdown.Item>
-            </DropdownButton>
-          </div>
-          <div className="space-y-1  ">
-            <DropdownButton variant="primary" title={openPV ? "Yes" : "No"}>
-              <Dropdown.Item onClick={() => setOpenPV(true)}>Yes</Dropdown.Item>
-              <Dropdown.Item onClick={() => setOpenPV(false)}>No</Dropdown.Item>
-            </DropdownButton>
-          </div>
-          <div className="space-y-1 ">
-            <DropdownButton variant="primary" title={studio ? "Yes" : "No"}>
-              <Dropdown.Item onClick={() => setStudio(true)}>Yes</Dropdown.Item>
-              <Dropdown.Item onClick={() => setStudio(false)}>No</Dropdown.Item>
-            </DropdownButton>
-          </div>
+      <input
+        value={size}
+        className="outline-none   rounded-md p-1   border-none hover:bg-gray-300 bg-white"
+        onChange={(e) => setSize(e.target.value)}
+        placeholder="Magazine"
+        style={{ width: "8%" }}
+      />
+
+      <div className="space-y-1 ">
+        <DropdownButton variant="primary" title={albumCrystal ? "Yes" : "No"}>
+          <Dropdown.Item onClick={() => setAlbumCrystal(true)}>
+            Yes
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => setAlbumCrystal(false)}>
+            No
+          </Dropdown.Item>
+        </DropdownButton>
+      </div>
+      <div className="space-y-1 ">
+        <DropdownButton variant="primary" title={magazineMini ? "Yes" : "No"}>
+          <Dropdown.Item onClick={() => setMagazineMini(true)}>
+            Yes
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => setMagazineMini(false)}>
+            No
+          </Dropdown.Item>
+        </DropdownButton>
+      </div>
+      <div className="space-y-1  ">
+        <DropdownButton variant="primary" title={video ? "Yes" : "No"}>
+          <Dropdown.Item onClick={() => setVideo(true)}>Yes</Dropdown.Item>
+          <Dropdown.Item onClick={() => setVideo(false)}>No</Dropdown.Item>
+        </DropdownButton>
+      </div>
+      <div className="space-y-1  ">
+        <DropdownButton variant="primary" title={openPV ? "Yes" : "No"}>
+          <Dropdown.Item onClick={() => setOpenPV(true)}>Yes</Dropdown.Item>
+          <Dropdown.Item onClick={() => setOpenPV(false)}>No</Dropdown.Item>
+        </DropdownButton>
+      </div>
+      <div className="space-y-1 ">
+        <DropdownButton variant="primary" title={studio ? "Yes" : "No"}>
+          <Dropdown.Item onClick={() => setStudio(true)}>Yes</Dropdown.Item>
+          <Dropdown.Item onClick={() => setStudio(false)}>No</Dropdown.Item>
+        </DropdownButton>
+      </div>
+
+      <input
+        value={price}
+        className="outline-none  mx-2   rounded-md p-1     border-none hover:bg-gray-300 bg-white"
+        onChange={(e) => setPrice(e.target.value)}
+        placeholder="Price"
+        style={{ width: "8%" }}
+      />
+      <div className="flex  items-center space-x-3">
+        <div className="">
+          <CheckCircleIcon
+            height={30}
+            width={30}
+            className="cursor-pointer"
+            color={checkHover}
+            onMouseEnter={() => setCheckHover("#047857")}
+            onMouseLeave={() => setCheckHover("#10b981")}
+            onClick={() => editRow()}
+          />
         </div>
-        <input
-          value={price}
-          className="outline-none  mx-2   rounded-md p-1     border-none hover:bg-gray-300 bg-white"
-          onChange={(e) => setPrice(e.target.value)}
-          placeholder="Price"
-          style={{ width: "8%" }}
-        />
-        <div className="flex  items-center space-x-3">
-          <div className="">
-            <CheckCircleIcon
-              height={30}
-              width={30}
-              className="cursor-pointer"
-              color={checkHover}
-              onMouseEnter={() => setCheckHover("#047857")}
-              onMouseLeave={() => setCheckHover("#10b981")}
-              onClick={() => editRow()}
+        <div className="">
+          <XCircleIcon
+            height={30}
+            width={30}
+            className="cursor-pointer"
+            color={xHover}
+            onMouseEnter={() => setXHover("black")}
+            onMouseLeave={() => setXHover("#334155")}
+            onClick={() => cancelRow()}
+          />
+        </div>
+        {props.new ? (
+          <div></div>
+        ) : (
+          <div>
+            <TrashIcon
+              height={20}
+              width={20}
+              color="gray"
+              className="cursor-pointer mx-auto  "
+              onClick={() => deleteRow()}
             />
           </div>
-          <div className="">
-            <XCircleIcon
-              height={30}
-              width={30}
-              className="cursor-pointer"
-              color={xHover}
-              onMouseEnter={() => setXHover("black")}
-              onMouseLeave={() => setXHover("#334155")}
-              onClick={() => cancelRow()}
-            />
-          </div>
-          {props.new ? (
-            <div></div>
-          ) : (
-            <div>
-              <TrashIcon
-                height={20}
-                width={20}
-                color="gray"
-                className="cursor-pointer mx-auto  "
-                onClick={() => deleteRow()}
-              />
-            </div>
-          )}
-        </div>
+        )}
       </div>
     </div>
   );
