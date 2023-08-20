f33bc1f5e4b5490cc076a1f7e2bad4301b514ef3
minaz99 <hanymina43@gmail.com>
2023-08-21 01:49:02 +0300

    skdnf

--- Webapp/julia-interface/src/Components/Packages/Package/PackageEditableRow.js
+++ Webapp/julia-interface/src/Components/Packages/Package/PackageEditableRow.js
@@ -69,21 +69,21 @@ function PackageEditableRow(props) {
     <div className="flex flex-row justify-evenly ">
       <input
         value={name}
-        className="outline-none w-1.5  rounded-md p-1      border-none hover:bg-gray-300 bg-white"
+        className="outline-none w-3.5  rounded-md p-1      border-none hover:bg-gray-300 bg-white"
         onChange={(e) => setName(e.target.value)}
         placeholder="Name"
       />
 
       <input
         value={pics}
-        className="outline-none w-1.5 max-w-sm mx-4 rounded-md  p-1    border-none hover:bg-gray-300 bg-white"
+        className="outline-none w-3.5  mx-4 rounded-md  p-1    border-none hover:bg-gray-300 bg-white"
         onChange={(e) => setPics(e.target.value)}
         placeholder="Pics #"
       />
 
       <input
         value={size}
-        className="outline-none w-fit max-w-sm   rounded-md p-1   border-none hover:bg-gray-300 bg-white"
+        className="outline-none w-3.5   rounded-md p-1   border-none hover:bg-gray-300 bg-white"
         onChange={(e) => setSize(e.target.value)}
         placeholder="Magazine"
       />
@@ -129,7 +129,7 @@ function PackageEditableRow(props) {
 
       <input
         value={price}
-        className="outline-none  mx-2  max-w-sm rounded-md p-1  w-fit   border-none hover:bg-gray-300 bg-white"
+        className="outline-none  mx-2  w-3.5 rounded-md p-1     border-none hover:bg-gray-300 bg-white"
         onChange={(e) => setPrice(e.target.value)}
         placeholder="Price"
       />
