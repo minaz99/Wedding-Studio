3fdad6fdbb43a20983add9fbc58019cd2e31e729
minaz99 <hanymina43@gmail.com>
2023-08-21 04:25:31 +0300

    skmdfslk

--- Webapp/julia-interface/src/Components/Login.js
+++ Webapp/julia-interface/src/Components/Login.js
@@ -22,7 +22,7 @@ function Login(props) {
   }, [result.isSuccess]);
 
   return (
-    <div className="w-screen bg-amber-50 h-fit p-12 ">
+    <div className="w-screen bg-amber-50 h-full p-12 ">
       <div className="bg-blue-200 rounded-xl w-fit mx-auto p-20 ">
         {result.isLoading ? (
           <Spinner animation="border" role="status"></Spinner>
