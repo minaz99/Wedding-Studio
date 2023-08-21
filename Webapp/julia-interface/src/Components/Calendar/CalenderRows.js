1137bc41eb4e229e1d6762797e8f5bf95f5e2fc8
minaz99 <hanymina43@gmail.com>
2023-08-21 05:03:17 +0300

    ksjdfn

--- Webapp/julia-interface/src/Components/Calendar/CalenderRows.js
+++ Webapp/julia-interface/src/Components/Calendar/CalenderRows.js
@@ -9,7 +9,7 @@ import DayContractDetails from "./DayContractDetails";
 
 function CalenderRows(props) {
   return (
-    <div className="flex items-center w-fit overflow-y-scroll rounded-md  bg-red-100   ">
+    <div className="flex items-center w-fit overflow-y-scroll mx-auto rounded-md  bg-red-100   ">
       <div className="cursor-pointer p-2 font-mono w-36  h-32 space-y-2   rounded-md  ">
         <div className="text-slate-600">{props.day}</div>
         <div className="space-y-2 pb-2">
