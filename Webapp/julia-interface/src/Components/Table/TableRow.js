5d4bb70d27ffed0e715c7fc8222ccb6d79923895
minaz99 <hanymina43@gmail.com>
2023-08-24 01:30:16 +0300

    kjndsf

--- Webapp/julia-interface/src/Components/Table/TableRow.js
+++ Webapp/julia-interface/src/Components/Table/TableRow.js
@@ -25,7 +25,7 @@ function TableRow(props) {
       className="border-t-2 border-r-2 flex items-center   bg-rose-50 shadow-md rounded-md my-1 border-l-2 border-gray-400 justify-center  text-black p-2 font-semibold "
       style={{ backgroundColor: rowColor }}
     >
-      <div className="flex flex-1 items-center">
+      <div className="flex  items-center">
         <UserIconWrapper letter={props.letter} color={userColor} />{" "}
         {props.brideName}
       </div>
