369f58233f2cc8419885b11f15a984fa3ce8c2c4
minaz99 <hanymina43@gmail.com>
2023-08-24 01:13:29 +0300

    sjnkjn

--- Webapp/julia-interface/src/Components/Table/TableRow.js
+++ Webapp/julia-interface/src/Components/Table/TableRow.js
@@ -22,15 +22,15 @@ function TableRow(props) {
   return (
     <div
       key={props.id}
-      className="border-t-2 border-r-2 flex items-center   bg-rose-50 shadow-md rounded-md my-1 border-l-2 border-gray-400  text-black p-2 font-semibold "
+      className="border-t-2 border-r-2 flex items-center   bg-rose-50 shadow-md rounded-md my-1 border-l-2 border-gray-400 justify-between  text-black p-2 font-semibold "
       style={{ backgroundColor: rowColor }}
     >
-      <div className="flex mx-10 flex-1 items-center">
+      <div className="flex flex-1 items-center">
         <UserIconWrapper letter={props.letter} color={userColor} />{" "}
         {props.brideName}
       </div>
-      <div className="flex-1 mx-10 ">{props.eventType}</div>
-      <div className="flex-1">{props.eventPlace}</div>
+      <div className="">{props.eventType}</div>
+      <div className="">{props.eventPlace}</div>
 
       <div className=" ">
         {/*<StatusWrapper status={props.status} />*/}
