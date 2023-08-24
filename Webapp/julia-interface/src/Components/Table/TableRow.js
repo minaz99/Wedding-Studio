8135bdac5b2d50deede7d94c06b26b2da27ba19f
minaz99 <hanymina43@gmail.com>
2023-08-24 03:09:10 +0300

    ksdfkjn

--- Webapp/julia-interface/src/Components/Table/TableRow.js
+++ Webapp/julia-interface/src/Components/Table/TableRow.js
@@ -25,31 +25,27 @@ function TableRow(props) {
       className="border-t-2 border-r-2 flex items-center   bg-rose-50 shadow-md rounded-md my-1 border-l-2 border-gray-400  text-black p-2 font-semibold "
       style={{ backgroundColor: rowColor }}
     >
-      <div className="flex-1 "></div>
-      <div className="flex  flex-1 items-center">
+      <div className="flex mx-14 flex-1 items-center">
         <UserIconWrapper letter={props.letter} color={userColor} />{" "}
         {props.brideName}
       </div>
       <div className="flex-1 ">{props.eventType}</div>
       <div className="flex-1">{props.eventPlace}</div>
 
-      <div className="flex-1">
-        {/*<StatusWrapper status={props.status} />*/}
-        {props.eventDate.toString().split("T")[0]}
+      <div className="flex-1 ">
+        {<StatusWrapper status={props.status} />}
+        {/*props.eventDate.toString().split("T")[0]*/}
       </div>
-      <div className="mx-20">
-        {
-          <EllipsisHorizontalCircleIcon
-            onClick={() => props.pressedRow(props.id)}
-            className="cursor-pointer"
-            height={30}
-            width={25}
-            onMouseEnter={() => setChevronColor("black")}
-            onMouseLeave={() => setChevronColor("gray")}
-            color={chevronColor}
-          />
-        }
-      </div>
+
+      <EllipsisHorizontalCircleIcon
+        onClick={() => props.pressedRow(props.id)}
+        className="cursor-pointer"
+        height={30}
+        width={25}
+        onMouseEnter={() => setChevronColor("black")}
+        onMouseLeave={() => setChevronColor("gray")}
+        color={chevronColor}
+      />
     </div>
   );
 }
