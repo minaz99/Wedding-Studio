13d76476301e3483da04205e866a2bf5a0257027
minaz99 <hanymina43@gmail.com>
2023-09-01 20:14:12 +0300

    Adding minus icon to each component + plus icon at the bottom

--- Webapp/julia-interface/src/Components/Contract/Contract Details/ComponentDelete.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/ComponentDelete.js
@@ -1,15 +1,34 @@
 import React from "react";
 import Addon from "./Addon";
+import { MinusCircleIcon, PlusCircleIcon } from "@heroicons/react/24/outline";
 
 function ComponentDelete(props) {
   let componentIDsArray = props.compsIDs.split(",");
   return (
-    <div className=" flex space-x-2">
+    <div className="space-y-3">
       {componentIDsArray
         .filter((c) => c !== "," && c !== "")
         .map((id) => {
-          return <Addon token={props.token} id={id} />;
+          return (
+            <div className="flex space-x-2">
+              <Addon token={props.token} id={id} />
+              <MinusCircleIcon
+                height={24}
+                width={24}
+                color="#475569"
+                className="cursor-pointer"
+              />
+            </div>
+          );
         })}
+      <div className="mx-auto">
+        <PlusCircleIcon
+          height={24}
+          width={24}
+          color="#475569"
+          className="cursor-pointer"
+        />
+      </div>
     </div>
   );
 }
