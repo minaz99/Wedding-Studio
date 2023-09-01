5084b85ee889104111a15d5549e33bc557aa8280
minaz99 <hanymina43@gmail.com>
2023-09-01 08:00:19 +0300

    adding edit package pencil and xcircle components

--- Webapp/julia-interface/src/Components/Contract/Contract Details/ContractPackagesWrapper.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/ContractPackagesWrapper.js
@@ -1,12 +1,33 @@
 import { CameraIcon } from "@heroicons/react/24/outline";
-import React from "react";
+import React, { useState } from "react";
 import ContractPackages from "./ContractPackages";
 import ContractComponents from "./ContractComponents";
+import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/solid";
 
 function ContractPackagesWrapper(props) {
+  const [editPackage, setEditPackage] = useState(false);
   return (
     <div className="space-y-3 rounded-lg p-4 w-full bg-white h-fit  ">
-      <div className="font-medium text-xl flex items-center ">Packages </div>
+      <div className="flex items-center">
+        <div className="font-medium text-xl flex-1">Packages</div>
+        {editPackage ? (
+          <XCircleIcon
+            height={22}
+            width={22}
+            color="#475569"
+            className="cursor-pointer"
+            onClick={() => setEditPackage(false)}
+          />
+        ) : (
+          <PencilSquareIcon
+            height={22}
+            width={22}
+            color="#475569"
+            className="cursor-pointer"
+            onClick={() => setEditPackage(true)}
+          />
+        )}
+      </div>
       <ContractPackages token={props.token} id={props.packageID} />
 
       <div className="flex space-x-6 items-center">
