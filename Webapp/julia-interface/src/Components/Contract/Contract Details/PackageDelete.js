cfe7e5aae65fa95db6790be923a9a057d9a26bb5
minaz99 <hanymina43@gmail.com>
2023-09-01 17:08:03 +0300

    changed the location of the minus icon + set pkg to string instead ot int

--- Webapp/julia-interface/src/Components/Contract/Contract Details/PackageDelete.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/PackageDelete.js
@@ -16,8 +16,20 @@ function PackageDelete(props) {
       {result.isSuccess ? (
         <div></div>
       ) : (
-        <div>
-          <div className="text-gray-800">Package</div>
+        <div className="space-x-3">
+          <MinusCircleIcon
+            height={22}
+            width={22}
+            color="#475569"
+            className="cursor-pointer"
+            onClick={() =>
+              updateContract({
+                token: props.token,
+                id: props.contractID,
+                body: { packageID: "0" },
+              })
+            }
+          />
           <div className="bg-blue-200 rounded-md  p-1 shadow-md">
             {isLoading ? (
               <div className="text-center text-blue-400 text-xl p-4">
@@ -28,36 +40,21 @@ function PackageDelete(props) {
                 Error loading packages
               </div>
             ) : isSuccess ? (
-              <div className="flex space-x-2">
-                <div>
-                  <div className="font-medium">{data.package.name}</div>
-                  <div className="text-gray-600 p-1 flex space-x-8">
-                    <li>{data.package.pictures} Pictures</li>
-                    <li>Magazine {data.package.magazinename}</li>
-                    <li>{data.package.albumcrystal ? "Album Crystal" : ""}</li>
-                    <li>{data.package.magazinemini ? "Mini Magazine" : ""}</li>
-                    <li>{data.package.video ? "Video" : ""}</li>
-                    <li>
-                      {data.package.openphotoandvideo
-                        ? "Open Photo and Video"
-                        : ""}
-                    </li>
-                    <li>{data.package.studio ? "Studio" : ""}</li>
-                  </div>
+              <div>
+                <div className="font-medium">{data.package.name}</div>
+                <div className="text-gray-600 p-1 flex space-x-8">
+                  <li>{data.package.pictures} Pictures</li>
+                  <li>Magazine {data.package.magazinename}</li>
+                  <li>{data.package.albumcrystal ? "Album Crystal" : ""}</li>
+                  <li>{data.package.magazinemini ? "Mini Magazine" : ""}</li>
+                  <li>{data.package.video ? "Video" : ""}</li>
+                  <li>
+                    {data.package.openphotoandvideo
+                      ? "Open Photo and Video"
+                      : ""}
+                  </li>
+                  <li>{data.package.studio ? "Studio" : ""}</li>
                 </div>
-                <MinusCircleIcon
-                  height={22}
-                  width={22}
-                  color="#475569"
-                  className="cursor-pointer"
-                  onClick={() =>
-                    updateContract({
-                      token: props.token,
-                      id: props.contractID,
-                      body: { packageID: 0 },
-                    })
-                  }
-                />
               </div>
             ) : (
               <div></div>
