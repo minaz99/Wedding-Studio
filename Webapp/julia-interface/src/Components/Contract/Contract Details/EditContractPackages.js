1116f4950cfd67029af028cb6cc4289d1638c3ff
minaz99 <hanymina43@gmail.com>
2023-09-01 13:37:52 +0300

    empty edit packages file

--- Webapp/julia-interface/src/Components/Contract/Contract Details/EditContractPackages.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/EditContractPackages.js
@@ -4,29 +4,7 @@ import ContractPackages from "./ContractPackages";
 import ContractComponents from "./ContractComponents";
 
 function EditContractPackages(props) {
-  return (
-    <div className="">
-      <div className="space-y-3">
-        <ContractPackages token={props.token} id={props.packageID} />
-
-        <div className="flex space-x-6 items-center">
-          <div className="text-gray-800">Add ons</div>
-          <ContractComponents token={props.token} compsIDs={props.compsIDs} />
-        </div>
-      </div>
-      <div className="space-y-3">
-        <div className="flex">
-          <div className="flex-1"></div>
-          <div className="space-x-2 flex items-center">
-            <div className="font-bold">Price</div>
-            <div className="rounded-md bg-blue-100 font-medium h-fit p-1">
-              {props.price} KD
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default EditContractPackages;
