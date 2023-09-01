d03825fe09014081a63c06f2238471fcda9d3ff0
minaz99 <hanymina43@gmail.com>
2023-09-01 13:04:20 +0300

    adding edit packages component empty so far

--- Webapp/julia-interface/src/Components/Contract/Contract Details/ContractPackagesWrapper.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/ContractPackagesWrapper.js
@@ -3,48 +3,55 @@ import React, { useState } from "react";
 import ContractPackages from "./ContractPackages";
 import ContractComponents from "./ContractComponents";
 import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/outline";
+import EditContractPackages from "./EditContractPackages";
 
 function ContractPackagesWrapper(props) {
   const [editPackage, setEditPackage] = useState(false);
   return (
     <div className="space-y-3 rounded-lg p-4 w-full bg-white h-fit  ">
-      <div className="flex items-center">
-        <div className="font-medium text-xl flex-1">Packages</div>
-        {editPackage ? (
-          <XCircleIcon
-            height={22}
-            width={22}
-            color="#475569"
-            className="cursor-pointer"
-            onClick={() => setEditPackage(false)}
-          />
-        ) : (
-          <PencilSquareIcon
-            height={22}
-            width={22}
-            color="#475569"
-            className="cursor-pointer"
-            onClick={() => setEditPackage(true)}
-          />
-        )}
-      </div>
-      <ContractPackages token={props.token} id={props.packageID} />
+      {editPackage ? (
+        <EditContractPackages />
+      ) : (
+        <div>
+          <div className="flex items-center">
+            <div className="font-medium text-xl flex-1">Packages</div>
+            {editPackage ? (
+              <XCircleIcon
+                height={22}
+                width={22}
+                color="#475569"
+                className="cursor-pointer"
+                onClick={() => setEditPackage(false)}
+              />
+            ) : (
+              <PencilSquareIcon
+                height={22}
+                width={22}
+                color="#475569"
+                className="cursor-pointer"
+                onClick={() => setEditPackage(true)}
+              />
+            )}
+          </div>
+          <ContractPackages token={props.token} id={props.packageID} />
 
-      <div className="flex space-x-6 items-center">
-        <div className="text-gray-800">Add ons</div>
-        <ContractComponents token={props.token} compsIDs={props.compsIDs} />
-      </div>
-      <div className="space-y-3">
-        <div className="flex">
-          <div className="flex-1"></div>
-          <div className="space-x-2 flex items-center">
-            <div className="font-bold">Price</div>
-            <div className="rounded-md bg-blue-100 font-medium h-fit p-1">
-              {props.price} KD
+          <div className="flex space-x-6 items-center">
+            <div className="text-gray-800">Add ons</div>
+            <ContractComponents token={props.token} compsIDs={props.compsIDs} />
+          </div>
+          <div className="space-y-3">
+            <div className="flex">
+              <div className="flex-1"></div>
+              <div className="space-x-2 flex items-center">
+                <div className="font-bold">Price</div>
+                <div className="rounded-md bg-blue-100 font-medium h-fit p-1">
+                  {props.price} KD
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
---
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/EditContractPackages.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EditContractPackages(props) {
+  return (
+    <div className="flex space-x-2 items-center">
+      <div className="text-gray-800">Package</div>
+    </div>
+  );
+}
+
+export default EditContractPackages;
