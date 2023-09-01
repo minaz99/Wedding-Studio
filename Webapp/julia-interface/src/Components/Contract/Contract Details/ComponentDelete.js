4aba002cffa8a2f81d2061e9facdbcdc581a18df
minaz99 <hanymina43@gmail.com>
2023-09-01 19:34:07 +0300

    Added delete comp component

---
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/ComponentDelete.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Addon from "./Addon";
+
+function ComponentDelete(props) {
+  let componentIDsArray = props.compsIDs.split(",");
+  return (
+    <div className=" flex space-x-2">
+      {componentIDsArray
+        .filter((c) => c !== "," && c !== "")
+        .map((id) => {
+          return <Addon token={props.token} id={id} />;
+        })}
+    </div>
+  );
+}
+
+export default ComponentDelete;
--- Webapp/julia-interface/src/Components/Contract/Contract Details/EditContractPackages.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/EditContractPackages.js
@@ -3,16 +3,18 @@ import React from "react";
 import ContractPackages from "./ContractPackages";
 import ContractComponents from "./ContractComponents";
 import PackageDelete from "./PackageDelete";
+import ComponentDelete from "./ComponentDelete";
 
 function EditContractPackages(props) {
   return (
-    <div>
+    <div className="space-y-3">
       <PackageDelete
         token={props.token}
         id={props.packageID}
         contractID={props.contractID}
         setPkgID={props.setPkgID}
       />
+      <ComponentDelete token={props.token} compsIDs={props.compsIDs} />
     </div>
   );
 }
