d7f931de2c887f7889786d9a4d7d348514db9ffb
minaz99 <hanymina43@gmail.com>
2023-09-01 14:35:42 +0300

    added token and id to delete package

--- Webapp/julia-interface/src/Components/Contract/Contract Details/EditContractPackages.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/EditContractPackages.js
@@ -2,9 +2,14 @@ import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import React from "react";
 import ContractPackages from "./ContractPackages";
 import ContractComponents from "./ContractComponents";
+import PackageDelete from "./PackageDelete";
 
 function EditContractPackages(props) {
-  return <div></div>;
+  return (
+    <div>
+      <PackageDelete token={props.token} id={props.packageID} />
+    </div>
+  );
 }
 
 export default EditContractPackages;
