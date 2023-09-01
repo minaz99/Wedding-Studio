0302e4784a1f10e466d0d776e5d5d5a5c1f9e41a
minaz99 <hanymina43@gmail.com>
2023-09-01 18:51:11 +0300

    A trial here

--- Webapp/julia-interface/src/Components/Contract/Contract Details/PackagesAndComponents.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/PackagesAndComponents.js
@@ -1,12 +1,54 @@
 import React from "react";
 import ContractPackages from "./ContractPackages";
 import ContractComponents from "./ContractComponents";
+import { useGetPackageByIDQuery } from "../../../services/api/packageSlice";
 
 function PackagesAndComponents(props) {
+  const { data, isLoading, isError, isSuccess } = useGetPackageByIDQuery(
+    { token: props.token, id: props.id },
+    {
+      refetchOnMountOrArgChange: true,
+    }
+  );
   return (
     <div className="space-y-3">
       <div className="space-y-3">
-        <ContractPackages token={props.token} id={props.packageID} />
+        <div className="bg-blue-200 rounded-md  p-1 shadow-md">
+          {isLoading ? (
+            <div className="text-center text-blue-400 text-xl p-4">
+              Loading...
+            </div>
+          ) : isError ? (
+            <div className="text-center text-red-400 text-xl p-4">
+              Error loading packages
+            </div>
+          ) : isSuccess ? (
+            <div>
+              {data.package === null ? (
+                <div></div>
+              ) : (
+                <div>
+                  <div className="font-medium">{data.package.name}</div>
+                  <div className="text-gray-600 p-1 flex space-x-8">
+                    <li>{data.package.pictures} Pictures</li>
+                    <li>Magazine {data.package.magazinename}</li>
+                    <li>{data.package.albumcrystal ? "Album Crystal" : ""}</li>
+                    <li>{data.package.magazinemini ? "Mini Magazine" : ""}</li>
+                    <li>{data.package.video ? "Video" : ""}</li>
+                    <li>
+                      {data.package.openphotoandvideo
+                        ? "Open Photo and Video"
+                        : ""}
+                    </li>
+                    <li>{data.package.studio ? "Studio" : ""}</li>
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
 
         <div className="flex space-x-6 items-center">
           <div className="text-gray-800">Add ons</div>
