11353495ece598d1a993f51270b06f47bdcbadc0
minaz99 <hanymina43@gmail.com>
2023-10-22 08:55:01 +0300

    fixed error for array of pkg items

--- Webapp/julia-interface/src/Components/Contract/New Contract Components/PackageSummary.js
+++ Webapp/julia-interface/src/Components/Contract/New Contract Components/PackageSummary.js
@@ -19,20 +19,20 @@ function PackageSummary(props) {
       ? props.packageDetails.pictures + "pictures"
       : ""
   );
-  props.packageDetails.albumcrystal ? pkgArray.push("Album Crystal") : "";
-  props.packageDetails.magazinemini ? pkgArray.push("Magazine Mini") : "";
-  props.packageDetails.video ? pkgArray.push("Video") : "";
-  props.packageDetails.openphotoandvideo
-    ? pkgArray.push("Open Photo and Video")
-    : "";
-  props.packageDetails.studio ? pkgArray.push("Studio") : "";
+  pkgArray.push(props.packageDetails.albumcrystal ? "Album Crystal" : "");
+  pkgArray.push(props.packageDetails.magazinemini ? "Magazine Mini" : "");
+  pkgArray.push(props.packageDetails.video ? "Video" : "");
+  pkgArray.push(
+    props.packageDetails.openphotoandvideo ? "Open Photo and Video" : ""
+  );
+  pkgArray.push(props.packageDetails.studio ? "Studio" : "");
 
   return (
     <div className="bg-amber-50  font-medium text-gray-500 rounded-md p-2">
       {Object.keys(props.packageDetails).length > 0 ? (
         <div>
           {pkgArray.map((item) => {
-            return <li>{item}</li>;
+            if (item !== "") return <li>{item}</li>;
           })}
         </div>
       ) : (
