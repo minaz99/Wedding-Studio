fb9f9aab2bc4f5a8b16e55f9f40205867fff1e90
minaz99 <hanymina43@gmail.com>
2023-10-22 17:36:34 +0300

    moved the outer div for the color of pkg to inside the isSuccess for the case of pkg is null

--- Webapp/julia-interface/src/Components/Contract/Contract Details/ContractPackages.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/ContractPackages.js
@@ -12,51 +12,48 @@ function ContractPackages(props) {
   return (
     <div className="flex space-x-2 items-center">
       <div className="text-gray-800">Package</div>
-      <div className="bg-blue-200 rounded-md  p-1 shadow-md">
-        {isLoading ? (
-          <div className="text-center text-blue-400 text-xl p-4">
-            Loading...
-          </div>
-        ) : isError ? (
-          <div className="text-center text-red-400 text-xl p-4">
-            Error loading packages
-          </div>
-        ) : isSuccess ? (
-          <div>
-            {data.package === null ? (
-              <div></div>
-            ) : (
-              <div>
-                <div className="font-medium">{data.package.name}</div>
-                <div className="text-gray-600 p-1 flex space-x-8">
-                  {data.package.magazinename &&
-                  data.package.magazinename !== "-" ? (
-                    <li>Magazine {data.package.magazinename}</li>
-                  ) : (
-                    ""
-                  )}
-                  {data.package.pictures ? (
-                    <li>{data.package.pictures} pictures</li>
-                  ) : (
-                    ""
-                  )}
-                  {data.package.albumcrystal ? <li>Album Crystal</li> : ""}
-                  {data.package.magazinemini ? <li>Magazine Mini</li> : ""}
-                  {data.package.video ? <li>Video</li> : ""}
-                  {data.package.openphotoandvideo ? (
-                    <li>Open Photo and Video</li>
-                  ) : (
-                    ""
-                  )}
-                  {data.package.studio ? <li>Studio</li> : ""}
-                </div>
+
+      {isLoading ? (
+        <div className="text-center text-blue-400 text-xl p-4">Loading...</div>
+      ) : isError ? (
+        <div className="text-center text-red-400 text-xl p-4">
+          Error loading packages
+        </div>
+      ) : isSuccess ? (
+        <div>
+          {data.package === null ? (
+            <div></div>
+          ) : (
+            <div className="bg-blue-200 rounded-md  p-1 shadow-md">
+              <div className="font-medium">{data.package.name}</div>
+              <div className="text-gray-600 p-1 flex space-x-8">
+                {data.package.magazinename &&
+                data.package.magazinename !== "-" ? (
+                  <li>Magazine {data.package.magazinename}</li>
+                ) : (
+                  ""
+                )}
+                {data.package.pictures ? (
+                  <li>{data.package.pictures} pictures</li>
+                ) : (
+                  ""
+                )}
+                {data.package.albumcrystal ? <li>Album Crystal</li> : ""}
+                {data.package.magazinemini ? <li>Magazine Mini</li> : ""}
+                {data.package.video ? <li>Video</li> : ""}
+                {data.package.openphotoandvideo ? (
+                  <li>Open Photo and Video</li>
+                ) : (
+                  ""
+                )}
+                {data.package.studio ? <li>Studio</li> : ""}
               </div>
-            )}
-          </div>
-        ) : (
-          <div></div>
-        )}
-      </div>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
