af1bbb09510ac90470b98a40be1788988630a506
minaz99 <hanymina43@gmail.com>
2023-10-23 18:55:14 +0300

    Added the new condition for checking of all stages and used new props (stages)

--- Webapp/julia-interface/src/Components/Contract/Contract Details/ContractStage.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/ContractStage.js
@@ -6,7 +6,7 @@ import {
 import React from "react";
 
 function ContractStage(props) {
-  const stages = ["Signed", "Event Finished", "Pics Collected", "Finished"];
+  //const stages = ["Signed", "Event Finished", "Pics Collected", "Finished"];
   const Check = () => {
     return (
       <div className="font-bold flex  text-gray-700 rounded-full bg-green-500 w-fit p-1 items-center justify-center font-mono">
@@ -17,7 +17,7 @@ function ContractStage(props) {
     );
   };
 
-  const InProgress = () => {
+  /*const InProgress = () => {
     return (
       <div className="font-bold flex rounded-full bg-blue-500 w-fit p-1 items-center justify-center font-mono">
         <div className="items-center ">
@@ -25,7 +25,7 @@ function ContractStage(props) {
         </div>
       </div>
     );
-  };
+  };*/
   const NotStarted = () => {
     return (
       <div className="font-bold flex rounded-full bg-gray-400 w-fit p-1 items-center justify-center font-mono">
@@ -36,26 +36,23 @@ function ContractStage(props) {
     );
   };
   const EventFinished = () => {
-    return stages.indexOf(props.stage) > 1 ? (
-      <Check />
-    ) : stages.indexOf(props.stage) >= 0 ? (
-      <InProgress />
-    ) : (
-      <NotStarted />
-    );
+    return props.stages.eventFinished ? <Check /> : <NotStarted />;
   };
 
   const PicsCollected = () => {
-    return stages.indexOf(props.stage) > 2 ? (
-      <Check />
-    ) : stages.indexOf(props.stage) === 2 ? (
-      <InProgress />
-    ) : (
-      <NotStarted />
-    );
+    return props.stages.picsCollected ? <Check /> : <NotStarted />;
   };
+
+  const VideoCollected = () => {
+    return props.stages.videoCollected ? <Check /> : <NotStarted />;
+  };
+
+  const PromoCollected = () => {
+    return props.stages.promoCollected ? <Check /> : <NotStarted />;
+  };
+
   const Finished = () => {
-    return stages.indexOf(props.stage) === 3 ? <Check /> : <NotStarted />;
+    return props.stages.finished ? <Check /> : <NotStarted />;
   };
 
   return (
@@ -73,11 +70,11 @@ function ContractStage(props) {
         <div className="text-gray-600  ">Pics Collected</div>
       </div>
       <div className="flex items-center space-x-2">
-        <PicsCollected />
+        <VideoCollected />
         <div className="text-gray-600  ">Video Collected</div>
       </div>
       <div className="flex items-center space-x-2">
-        <PicsCollected />
+        <PromoCollected />
         <div className="text-gray-600  ">Promo Collected</div>
       </div>
       <div className="flex items-center space-x-2">
