b702fc8b40eb4d3b822d0acfb41175e423b53e41
minaz99 <hanymina43@gmail.com>
2023-10-24 04:10:57 +0300

    changed color of text -> slate-700 along with color of icons

--- Webapp/julia-interface/src/Components/Calendar/DayContractDetails.js
+++ Webapp/julia-interface/src/Components/Calendar/DayContractDetails.js
@@ -7,18 +7,18 @@ import React from "react";
 
 function DayContractDetails(props) {
   return (
-    <div className="rounded-md p-1  border-slate-300 border-2">
+    <div className="rounded-md p-1  border-slate-400 border-2">
       <div className="flex items-center space-x-2 ">
-        <CameraIcon height={16} width={16} color="#475569" />
-        <div className="text-sm  text-slate-600">{props.photographer}</div>
+        <CameraIcon height={16} width={16} color="#334155" />
+        <div className="text-sm  text-slate-700">{props.photographer}</div>
       </div>
       <div className="flex items-center space-x-2 ">
-        <VideoCameraIcon height={16} width={16} color="#475569" />
-        <div className="text-sm  text-slate-600">{props.video}</div>
+        <VideoCameraIcon height={16} width={16} color="#334155" />
+        <div className="text-sm  text-slate-700">{props.video}</div>
       </div>
       <div className="flex items-center space-x-2 ">
-        <BuildingStorefrontIcon height={16} width={16} color="#475569" />
-        <div className="text-sm  text-slate-600">{props.location}</div>
+        <BuildingStorefrontIcon height={16} width={16} color="#334155" />
+        <div className="text-sm  text-slate-700">{props.location}</div>
       </div>
     </div>
   );
