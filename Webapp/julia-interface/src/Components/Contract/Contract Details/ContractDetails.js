f4c49bd4a2a0a1e0c432fd4be740952a1e1edfbf
minaz99 <hanymina43@gmail.com>
2023-10-24 19:16:44 +0300

    trial 4

--- Webapp/julia-interface/src/Components/Contract/Contract Details/ContractDetails.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/ContractDetails.js
@@ -37,22 +37,22 @@ function ContractDetails(props) {
         <div className="flex flex-1 space-x-2">
           <UserIcon height={22} width={22} color="#ec4899" />
           <div className="text-gray-500">Bride</div>
-          <div className="rounded-full">{data.bridename}</div>
+          <div className="rounded-full">{data.contract.bridename}</div>
         </div>
         <div className="flex space-x-2">
           <UserIcon height={22} width={22} color="#3b82f6" />
           <div className="text-gray-500">Groom</div>
-          <div>{data.groomname}</div>
+          <div>{data.contract.groomname}</div>
         </div>
-        {/*<div className="flex space-x-2">
+        <div className="flex space-x-2">
           <UserGroupIcon height={22} width={22} color="#78716c" />
           <div className="text-gray-500">Second Party</div>
-          <div>{data.secondpartyname}</div>
+          <div>{data.contract.secondpartyname}</div>
         </div>
         <div className="flex space-x-2 flex-1">
           <UserCircleIcon height={22} width={22} color="#fbbf24" />
           <div className="text-gray-500">Created by</div>
-          <div>{data.createdby}</div>
+          <div>{data.contract.createdby}</div>
         </div>
       </div>
 
@@ -60,25 +60,25 @@ function ContractDetails(props) {
         <div className="flex space-x-2 flex-1">
           <MegaphoneIcon height={22} width={22} color="#a78bfa" />
           <div className="text-gray-500">Type</div>
-          <div>{data.eventtype}</div>
+          <div>{data.contract.eventtype}</div>
         </div>
         <div className="flex space-x-2 flex-1">
           <BuildingStorefrontIcon height={22} width={22} color="#2dd4bf" />
           <div className="text-gray-500">Location</div>
-          <div>{data.eventlocation}</div>
+          <div>{data.contract.eventlocation}</div>
         </div>
         <div className="flex space-x-2 flex-1">
           <CalendarDaysIcon height={22} width={22} color="#818cf8" />
           <div className="text-gray-500">Event Date</div>
-          <div>{data.eventdate.toString().split("T")[0]}</div>
+          <div>{data.contract.eventdate.toString().split("T")[0]}</div>
         </div>
         <div className="flex space-x-2 flex-1">
           <CalendarDaysIcon height={22} width={22} color="#818cf8" />
           <div className="text-gray-500">Date Created</div>
-          <div>{data.datecreated.toString().split("T")[0]}</div>
+          <div>{data.contract.datecreated.toString().split("T")[0]}</div>
         </div>
       </div>
-
+      {/*
       <div className="space-y-3">
         <div className="flex space-x-2 flex-1">
           <IdentificationIcon height={22} width={22} color="#475569" />
@@ -153,7 +153,6 @@ function ContractDetails(props) {
           <div></div>
         )}
         </div>*/}
-      </div>
     </div>
   ) : (
     <div></div>
