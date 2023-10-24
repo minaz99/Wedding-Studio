b8dbdd81c7f7c44784c2483ed0370ceb28602e8f
minaz99 <hanymina43@gmail.com>
2023-10-24 04:00:28 +0300

    changed the heavy pink borders and text for event location and photographers

--- Webapp/julia-interface/src/Components/Calendar/DayContractDetails.js
+++ Webapp/julia-interface/src/Components/Calendar/DayContractDetails.js
@@ -7,18 +7,18 @@ import React from "react";
 
 function DayContractDetails(props) {
   return (
-    <div className="rounded-md p-1  border-pink-300 border-2">
+    <div className="rounded-md p-1  border-slate-300 border-2">
       <div className="flex items-center space-x-2 ">
         <CameraIcon height={16} width={16} color="#db2777" />
-        <div className="text-sm  text-pink-600">{props.photographer}</div>
+        <div className="text-sm  text-slate-600">{props.photographer}</div>
       </div>
       <div className="flex items-center space-x-2 ">
         <VideoCameraIcon height={16} width={16} color="#db2777" />
-        <div className="text-sm  text-pink-600">{props.video}</div>
+        <div className="text-sm  text-slate-600">{props.video}</div>
       </div>
       <div className="flex items-center space-x-2 ">
         <BuildingStorefrontIcon height={16} width={16} color="#db2777" />
-        <div className="text-sm  text-pink-600">{props.location}</div>
+        <div className="text-sm  text-slte-600">{props.location}</div>
       </div>
     </div>
   );
