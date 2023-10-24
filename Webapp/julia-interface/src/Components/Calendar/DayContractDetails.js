9ba8768db274f8401f11de5c08089461b5b42b6e
minaz99 <hanymina43@gmail.com>
2023-10-24 04:07:33 +0300

    changed the color of the icons for each day in the calendar cell

--- Webapp/julia-interface/src/Components/Calendar/DayContractDetails.js
+++ Webapp/julia-interface/src/Components/Calendar/DayContractDetails.js
@@ -9,15 +9,15 @@ function DayContractDetails(props) {
   return (
     <div className="rounded-md p-1  border-slate-300 border-2">
       <div className="flex items-center space-x-2 ">
-        <CameraIcon height={16} width={16} color="#db2777" />
+        <CameraIcon height={16} width={16} color="#475569" />
         <div className="text-sm  text-slate-600">{props.photographer}</div>
       </div>
       <div className="flex items-center space-x-2 ">
-        <VideoCameraIcon height={16} width={16} color="#db2777" />
+        <VideoCameraIcon height={16} width={16} color="#475569" />
         <div className="text-sm  text-slate-600">{props.video}</div>
       </div>
       <div className="flex items-center space-x-2 ">
-        <BuildingStorefrontIcon height={16} width={16} color="#db2777" />
+        <BuildingStorefrontIcon height={16} width={16} color="#475569" />
         <div className="text-sm  text-slate-600">{props.location}</div>
       </div>
     </div>
