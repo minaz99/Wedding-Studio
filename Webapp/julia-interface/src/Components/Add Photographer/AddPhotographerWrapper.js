e4566d1f17af452b303402e0ec346d53d2e35314
minaz99 <hanymina43@gmail.com>
2023-10-26 05:43:46 +0300

    added motion and animation to the wrapper

--- Webapp/julia-interface/src/Components/Add Photographer/AddPhotographerWrapper.js
+++ Webapp/julia-interface/src/Components/Add Photographer/AddPhotographerWrapper.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 function AddPhotographerWrapper() {
-  return <div>AddPhotographerWrapper</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0.25, x: -200, y: 0, scale: 1.2 }} //x:200 ,x:0
+      animate={{
+        opacity: 1,
+        y: 0,
+        x: 0,
+        scale: 1,
+      }}
+      transition={{ duration: 0.75 }}
+      className="rounded-r-lg  space-y-6  p-3 items-center h-full w-11/12 shadow-md overflow-y-scroll  bg-slate-600"
+    ></motion.div>
+  );
 }
 
 export default AddPhotographerWrapper;
