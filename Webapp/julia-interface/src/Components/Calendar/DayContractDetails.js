addc393ce536124fe39961514b8f0a5f69bf6394
minaz99 <hanymina43@gmail.com>
2023-10-26 05:04:00 +0300

    swapping order moved location as the 1st in order

--- Webapp/julia-interface/src/Components/Calendar/DayContractDetails.js
+++ Webapp/julia-interface/src/Components/Calendar/DayContractDetails.js
@@ -13,6 +13,10 @@ function DayContractDetails(props) {
   return (
     <div className="rounded-md p-1  border-slate-400 border-2">
       <div className="flex items-center space-x-2 ">
+        <BuildingStorefrontIcon height={16} width={16} color="#334155" />
+        <div className="text-sm  text-slate-700">{props.location}</div>
+      </div>
+      <div className="flex items-center space-x-2 ">
         <CameraIcon height={16} width={16} color="#334155" />
         <div className="text-sm  text-slate-700">{props.photographer}</div>
       </div>
@@ -52,10 +56,6 @@ function DayContractDetails(props) {
       ) : (
         <div></div>
       )}
-      <div className="flex items-center space-x-2 ">
-        <BuildingStorefrontIcon height={16} width={16} color="#334155" />
-        <div className="text-sm  text-slate-700">{props.location}</div>
-      </div>
     </div>
   );
 }
