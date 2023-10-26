7567e6dfbe24481c03a0cba091ad2b2ecb5fc537
minaz99 <hanymina43@gmail.com>
2023-10-26 18:57:58 +0300

    made some changes in moving the closing of div to be wrapped around all the items

--- Webapp/julia-interface/src/Components/Add Photographer/AddPhotographerWrapper.js
+++ Webapp/julia-interface/src/Components/Add Photographer/AddPhotographerWrapper.js
@@ -34,66 +34,69 @@ function AddPhotographerWrapper() {
     >
       <div className="bg-white rounded-lg">
         <div className="flex space-x-2">
-          <div className="font-extrabold text-xl text-slate-600">
+          <div className="font-extrabold text-xl items-center text-slate-600">
             Add Photographer
           </div>
-          <UserPlusIcon height={18} width={18} color="#475569" />
-        </div>
-        <div className="flex space-x-2">
-          <div className="text-black font-medium ">Name</div>
-          <div className="rounded-md bg-slate-200 w-fit space-x-2 flex items-center p-1">
-            <UserIcon height={22} width={22} color="#ec4899" />
-            <input
-              value={name}
-              style={{
-                background: "#e2e8f0",
-                borderRadius: "6px",
-                color: "#475569",
-                padding: "3px 3px 3px 3px",
-                border: "none",
-                outline: "none",
-              }}
-              className="font-medium "
-              onChange={(e) =>
-                setName(
-                  e.target.value.charAt(0).toUpperCase() +
-                    e.target.value.slice(1)
-                )
-              }
-            />
-          </div>
-        </div>
-        <div className="text-black font-medium">Type</div>
+          <UserPlusIcon height={20} width={20} color="#475569" />
 
-        <div className="rounded-md bg-slate-200 w-fit space-x-2 flex items-center p-1">
-          <CameraIcon height={22} width={22} color="#db2777" />
-          <DropdownButton id="dropdown-basic-button" title={type}>
-            {types.map((type) => {
-              return (
-                <Dropdown.Item onClick={() => setType(type)}>
-                  {type}
-                </Dropdown.Item>
-              );
-            })}
-          </DropdownButton>
-          <div>
-            <div className="text-black font-medium ">Date</div>
+          <div className="flex space-x-2 items-center">
+            <div className="text-black font-medium ">Name</div>
             <div className="rounded-md bg-slate-200 w-fit space-x-2 flex items-center p-1">
-              <CalendarDaysIcon height={22} width={22} color="#818cf8" />
+              <UserIcon height={22} width={22} color="#ec4899" />
               <input
-                type="date"
-                value={date}
+                value={name}
                 style={{
                   background: "#e2e8f0",
                   borderRadius: "6px",
                   color: "#475569",
-                  padding: "4px 4px 4px 4px",
+                  padding: "3px 3px 3px 3px",
                   border: "none",
                   outline: "none",
                 }}
-                className="font-medium"
-                onChange={(e) => setDate(e.target.value)}
+                className="font-medium "
+                onChange={(e) =>
+                  setName(
+                    e.target.value.charAt(0).toUpperCase() +
+                      e.target.value.slice(1)
+                  )
+                }
               />
+            </div>
+          </div>
+          <div className="flex space-x-2 items-center">
+            <div className="text-black font-medium">Type</div>
+
+            <div className="rounded-md bg-slate-200 w-fit space-x-2 flex items-center p-1">
+              <CameraIcon height={22} width={22} color="#db2777" />
+              <DropdownButton id="dropdown-basic-button" title={type}>
+                {types.map((type) => {
+                  return (
+                    <Dropdown.Item onClick={() => setType(type)}>
+                      {type}
+                    </Dropdown.Item>
+                  );
+                })}
+              </DropdownButton>
+            </div>
+            <div className="flex space-x-2 items-center">
+              <div className="text-black font-medium ">Date</div>
+              <div className="rounded-md bg-slate-200 w-fit space-x-2 flex items-center p-1">
+                <CalendarDaysIcon height={22} width={22} color="#818cf8" />
+                <input
+                  type="date"
+                  value={date}
+                  style={{
+                    background: "#e2e8f0",
+                    borderRadius: "6px",
+                    color: "#475569",
+                    padding: "4px 4px 4px 4px",
+                    border: "none",
+                    outline: "none",
+                  }}
+                  className="font-medium"
+                  onChange={(e) => setDate(e.target.value)}
+                />
+              </div>
             </div>
           </div>
         </div>
