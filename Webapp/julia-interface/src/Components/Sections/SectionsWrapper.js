a7f7e667860e529733c67fb7010cbe51506711e1
minaz99 <hanymina43@gmail.com>
2023-10-26 05:34:30 +0300

    added the userplusicon import

--- Webapp/julia-interface/src/Components/Sections/SectionsWrapper.js
+++ Webapp/julia-interface/src/Components/Sections/SectionsWrapper.js
@@ -7,6 +7,7 @@ import {
   PlusCircleIcon,
   TableCellsIcon,
   UserCircleIcon,
+  UserPlusIcon,
 } from "@heroicons/react/24/outline";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
