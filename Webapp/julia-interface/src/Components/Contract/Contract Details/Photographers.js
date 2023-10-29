0caf9e099aa2acfd303245c2866625a9b5821732
minaz99 <hanymina43@gmail.com>
2023-10-29 18:55:07 +0300

    added the endpoint for getting photographers for a contract

--- Webapp/julia-interface/src/Components/Contract/Contract Details/Photographers.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/Photographers.js
@@ -17,21 +17,30 @@ function Photographers(props) {
       Error loading photographers
     </div>
   ) : (
-    data.Photographers.map((photographer) => {
-      return photographer ? (
-        <div className="flex space-x-2 flex-1">
-          {props.type === "Photographer" ? (
+    <div className="flex space-x-2">
+      {data.photographers.map((photographer) => {
+        return photographer ? (
+          <div className="space-y-2 flex space-x-2 flex-1">
             <CameraIcon height={22} width={22} color="#db2777" />
-          ) : (
+            <div className="text-gray-500">{video.type}</div>
+            <div>{photographer.name}</div>
+          </div>
+        ) : (
+          <div></div>
+        );
+      })}
+      {data.video.map((video) => {
+        return video ? (
+          <div className="flex space-x-2 space-y-2 flex-1">
             <VideoCameraIcon height={22} width={22} color="#db2777" />
-          )}
-          <div className="text-gray-500">{props.type}</div>
-          <div>{photographer.name}</div>
-        </div>
-      ) : (
-        <div></div>
-      );
-    })
+            <div className="text-gray-500">{video.type}</div>
+            <div>{video.name}</div>
+          </div>
+        ) : (
+          <div></div>
+        );
+      })}
+    </div>
   );
 }
 
