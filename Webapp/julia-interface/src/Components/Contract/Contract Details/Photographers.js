cb3acc8ee54434f27a9282db6e47929f3a7df839
minaz99 <hanymina43@gmail.com>
2023-10-29 23:35:42 +0300

    changed justify from around -> start

--- Webapp/julia-interface/src/Components/Contract/Contract Details/Photographers.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/Photographers.js
@@ -14,7 +14,7 @@ function Photographers(props) {
       Error loading photographers
     </div>
   ) : (
-    <div className="flex justify-around">
+    <div className="flex justify-start">
       <div className="space-y-2">
         {data.photographers.map((photographer) => {
           return photographer ? (
