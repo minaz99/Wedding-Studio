de3367d4f1d0df86f0ca4c3769375a811caeb5ef
minaz99 <hanymina43@gmail.com>
2023-10-29 23:14:49 +0300

    made small changes in the alignment

--- Webapp/julia-interface/src/Components/Contract/Contract Details/Photographers.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/Photographers.js
@@ -15,28 +15,32 @@ function Photographers(props) {
     </div>
   ) : (
     <div className="flex space-x-2">
-      {data.photographers.map((photographer) => {
-        return photographer ? (
-          <div className="space-y-2 flex space-x-2 flex-1">
-            <CameraIcon height={22} width={22} color="#db2777" />
-            <div className="text-gray-500">{photographer.type}</div>
-            <div>{photographer.name}</div>
-          </div>
-        ) : (
-          <div></div>
-        );
-      })}
-      {data.video.map((video) => {
-        return video ? (
-          <div className="flex space-x-2 space-y-2 flex-1">
-            <VideoCameraIcon height={22} width={22} color="#db2777" />
-            <div className="text-gray-500">{video.type}</div>
-            <div>{video.name}</div>
-          </div>
-        ) : (
-          <div></div>
-        );
-      })}
+      <div className="space-y-2">
+        {data.photographers.map((photographer) => {
+          return photographer ? (
+            <div className="space-y-2 flex items-center space-x-2 flex-1">
+              <CameraIcon height={22} width={22} color="#db2777" />
+              <div className="text-gray-500">{photographer.type}</div>
+              <div>{photographer.name}</div>
+            </div>
+          ) : (
+            <div></div>
+          );
+        })}
+      </div>
+      <div>
+        {data.video.map((video) => {
+          return video ? (
+            <div className="flex space-x-2 space-y-2 flex-1">
+              <VideoCameraIcon height={22} width={22} color="#db2777" />
+              <div className="text-gray-500">{video.type}</div>
+              <div>{video.name}</div>
+            </div>
+          ) : (
+            <div></div>
+          );
+        })}
+      </div>
     </div>
   );
 }
