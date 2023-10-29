c5fa13ad652261a728e40d87d5b294b76f4d7c9e
minaz99 <hanymina43@gmail.com>
2023-10-29 12:45:48 +0300

    delete button beside the drop down button

--- Webapp/julia-interface/src/Components/Table/Contracts Fetching Components/GetAvailablePhotographersForContract.js
+++ Webapp/julia-interface/src/Components/Table/Contracts Fetching Components/GetAvailablePhotographersForContract.js
@@ -3,6 +3,7 @@ import { useGetPhotographersByTypeAndDateQuery } from "../../../services/api/pho
 import { useSetPhotographerToContractMutation } from "../../../services/api/contractSlice";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import { XCircleIcon } from "@heroicons/react/24/outline";
 function GetAvailablePhotographersForContract(props) {
   const { data, isLoading, isError, isSuccess } =
     useGetPhotographersByTypeAndDateQuery(
@@ -26,33 +27,36 @@ function GetAvailablePhotographersForContract(props) {
 
   //alert(`Date: ${props.date.toString().split("T")[0]} and type: ${props.type}`);
   return (
-    <DropdownButton id="dropdown-basic-button" title={props.photographer}>
-      {isLoading ? (
-        <div className="text-center text-blue-400 text-xl p-4">Loading</div>
-      ) : isError ? (
-        <div className="text-center text-red-400 text-xl p-4">
-          Error loading photographers
-        </div>
-      ) : result.isLoading ? (
-        <div className="text-center text-blue-400 text-xl p-4">Saving</div>
-      ) : result.isError ? (
-        <div className="text-center text-red-400 text-xl p-4">
-          Error setting photographer
-        </div>
-      ) : (
-        data.Photographers.map((photographer) => {
-          return (
-            <Dropdown.Item
-              onClick={async () =>
-                savePhotographer(photographer.id, photographer.name)
-              }
-            >
-              {photographer.name}
-            </Dropdown.Item>
-          );
-        })
-      )}{" "}
-    </DropdownButton>
+    <div className="flex space-x-2">
+      <DropdownButton id="dropdown-basic-button" title={props.photographer}>
+        {isLoading ? (
+          <div className="text-center text-blue-400 text-xl p-4">Loading</div>
+        ) : isError ? (
+          <div className="text-center text-red-400 text-xl p-4">
+            Error loading photographers
+          </div>
+        ) : result.isLoading ? (
+          <div className="text-center text-blue-400 text-xl p-4">Saving</div>
+        ) : result.isError ? (
+          <div className="text-center text-red-400 text-xl p-4">
+            Error setting photographer
+          </div>
+        ) : (
+          data.Photographers.map((photographer) => {
+            return (
+              <Dropdown.Item
+                onClick={async () =>
+                  savePhotographer(photographer.id, photographer.name)
+                }
+              >
+                {photographer.name}
+              </Dropdown.Item>
+            );
+          })
+        )}{" "}
+      </DropdownButton>
+      <XCircleIcon height="20" width="20" color="#64748b" />
+    </div>
   );
 }
 
