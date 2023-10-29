eb9d00b14b855d73b9203d7a2ea8a5b4a14a2509
minaz99 <hanymina43@gmail.com>
2023-10-29 23:44:11 +0300

    added remaining video types

--- Webapp/julia-interface/src/Components/Contract/Contract Details/Photographers.js
+++ Webapp/julia-interface/src/Components/Contract/Contract Details/Photographers.js
@@ -14,7 +14,7 @@ function Photographers(props) {
       Error loading photographers
     </div>
   ) : (
-    <div className="flex space-x-4">
+    <div className="flex space-x-8">
       <div className="space-y-2">
         {data.photographers.map((photographer) => {
           return photographer ? (
@@ -35,6 +35,58 @@ function Photographers(props) {
               <VideoCameraIcon height={22} width={22} color="#db2777" />
               <div className="text-gray-500">{video.type}</div>
               <div>{video.name}</div>
+            </div>
+          ) : (
+            <div></div>
+          );
+        })}
+      </div>
+      <div className="space-y-2">
+        {data.zoomLight.map((zl) => {
+          return zl ? (
+            <div className="flex space-x-2 items-center">
+              <VideoCameraIcon height={22} width={22} color="#db2777" />
+              <div className="text-gray-500">{zl.type}</div>
+              <div>{zl.name}</div>
+            </div>
+          ) : (
+            <div></div>
+          );
+        })}
+      </div>
+      <div className="space-y-2">
+        {data.cameraCrane.map((cr) => {
+          return cr ? (
+            <div className="flex space-x-2 items-center">
+              <VideoCameraIcon height={22} width={22} color="#db2777" />
+              <div className="text-gray-500">{cr.type}</div>
+              <div>{cr.name}</div>
+            </div>
+          ) : (
+            <div></div>
+          );
+        })}
+      </div>
+      <div className="space-y-2">
+        {data.hangingCamera.map((hc) => {
+          return hc ? (
+            <div className="flex space-x-2 items-center">
+              <VideoCameraIcon height={22} width={22} color="#db2777" />
+              <div className="text-gray-500">{hc.type}</div>
+              <div>{hc.name}</div>
+            </div>
+          ) : (
+            <div></div>
+          );
+        })}
+      </div>
+      <div className="space-y-2">
+        {data.cameraRonin.map((cr) => {
+          return cr ? (
+            <div className="flex space-x-2 items-center">
+              <VideoCameraIcon height={22} width={22} color="#db2777" />
+              <div className="text-gray-500">{cr.type}</div>
+              <div>{cr.name}</div>
             </div>
           ) : (
             <div></div>
